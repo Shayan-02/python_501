ca351045cfb4ffcc3ec064701b0ae47217b93dc1
Shayan Shahbazi <upm200@gmail.com>
2024-08-05 15:28:11 +0330

    j05

--- j04_exercise/3.py
+++
@@ -1,7 +0,0 @@
-r = float(input("enter circle radius: "))
-
-p = 2*r*3.14
-a = r*r*3.14
-
-print("perimeter: ", p)
-print("area: ", a)
--- j04_exercise/8.py
+++
@@ -1,9 +0,0 @@
-temp = int(input("enter a number: "))
-
-if temp < 0:
-    print("ice")
-else:
-    if temp < 100:
-        print("water")
-    else:
-        print("steam")
